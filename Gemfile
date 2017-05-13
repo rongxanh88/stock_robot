affb810fab2a0222bbca6becbcb6224257a3c0eb
rongxanh88 <rongxanh88@gmail.com>
2017-05-13 15:22:04 -0600

    add database cleaner

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development, :test do
   gem 'capybara', '~> 2.13.0'
   gem 'selenium-webdriver'
   gem 'rspec-rails'
+  gem 'database_cleaner'
 end
 
 group :development do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,21 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'capybara/dsl'
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :truncation
+
 RSpec.configure do |config|
+  config.include Capybara::DSL
+
+  config.before(:all) do
+    DatabaseCleaner.clean 
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean 
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
