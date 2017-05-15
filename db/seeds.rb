eff9d1b9664fce8e12a3233249599f7ec6dee939
rongxanh88 <rongxanh88@gmail.com>
2017-05-15 17:40:43 -0600

    success in seeding sector, industry, security, and ticker tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,28 +11,21 @@ class Seed
   def self.single_quote_request
     file = "db/csv/complete_stock_ticker.csv"
     quoter = Quote.new
-    counter = 1
-    limit = 5
+
     CSV.foreach file, headers: true, header_converters: :symbol do |row|
       json = quoter.get_quote(row[:symbol])
       first_quote = json["quotes"]["quote"]
-
-      ticker = Ticker.create!(symbol: row[:symbol], description: row[:name])
 
       sector = Sector.find_or_create_by(name: row[:sector])
       industry = Industry.find_or_create_by(name: row[:industry])
       security = Security.find_or_create_by(
         security_type: first_quote["type"]
         )
-      binding.pry
-      ticker = Ticker.create!(
-        symbol: row[:symbol], description: row[:name], sector_id: sector.id,
-        industry_id: industry.id, security_id: security.id
+
+      ticker = Ticker.create!(symbol: row[:symbol], description: row[:name])
+      ticker.update_attributes(
+        security_id: security.id, sector_id: sector.id, industry_id: industry.id
         )
-
-
-      counter += 1
-      break if counter > limit
     end
     
   end
