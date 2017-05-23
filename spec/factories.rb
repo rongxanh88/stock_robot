cf0493201a75c971e2d369b15403b61ea3f6e3b0
rongxanh88 <rongxanh88@gmail.com>
2017-05-23 16:02:40 -0600

    add historical date model spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,10 @@
 FactoryGirl.define do
-  
+
+  factory :historical_date do
+    sequence :date do |n|
+      "#{DateTime.new + n}"
+    end
+  end
+
+
 end
--- spec/models/historical_date_spec.rb
+++ spec/models/historical_date_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe HistoricalDate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "valid attributes" do
+    it { should validate_presence_of(:date) }
+  end
+
+  describe "relationships" do
+    it "has many trading data" do
+      date = create(:historical_date)
+      expect(date).to respond_to(:trading_data)
+    end
+
+    it "has many tickers" do
+      date = create(:historical_date)
+      expect(date).to respond_to(:tickers)
+    end
+  end
 end
