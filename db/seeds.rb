ce448c41177c1aeb7586f8c91e509c3aa95d2bb3
rongxanh88 <rongxanh88@gmail.com>
2017-05-23 15:27:27 -0600

    finish refactor of seed file. Seeds 6200 rows

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-require 'pry'
 require 'csv'
 require './lib/quote'
 require './app/models/ticker'
@@ -7,41 +6,40 @@ require './app/models/historical_date'
 require './app/models/trading_data'
 
 class Seed
-  attr_accessor :start_time
   attr_reader :RATE_LIMIT
 
   def initialize
     @RATE_LIMIT = 119
-    @start_time = 0
   end
 
   def self.quote_request
     file = "db/csv/complete_stock_ticker.csv"
 
-    @start_time = Time.now
     counter = 0
     ticker_symbols = []
 
     CSV.foreach file, headers: true, header_converters: :symbol do |row|
+      if row[:symbol].include? "^"
+        next
+      end
+
       sector = Sector.find_or_create_by(name: row[:sector])
       industry = Industry.find_or_create_by(name: row[:industry])
-      ticker = Ticker.create!(symbol: row[:symbol], description: row[:name])
+      ticker = Ticker.find_or_create_by(symbol: row[:symbol].strip, description: row[:name])
 
       ticker.update_attributes(
         sector_id: sector.id, industry_id: industry.id
         )
       ticker_symbols.push(row[:symbol])
 
-      if ticker_symbols.size >= 150
+      if ticker_symbols.size >= 50
         fill_ticker_data(ticker_symbols)
       end
 
-      counter = check_call_limit(counter)
+      counter += 1
 
-      #fix nil class problem
-      puts "Time: #{Time.now} and counter: #{counter}"
+      puts "Row: #{counter}"
     end
-    # 6700 rows of the CSV are added in one minute
     fill_ticker_data(ticker_symbols) if !ticker_symbols.empty?
   end
 
@@ -51,22 +49,6 @@ class Seed
     securities = symbols_to_string(ticker_symbols)
     ticker_symbols.clear
     make_json_request(securities)
-  end
-
-  def self.check_call_limit(counter)
-    counter += 1
-    current_time = Time.now
-    time_left = (@start_time + 60) - current_time
-
-    if counter == @RATE_LIMIT
-      sleep(time_left)
-    end
-
-    if time_left <= 0
-      counter = 0
-      @start_time = Time.now
-    end
-    counter
   end
 
   def self.make_json_request(ticker_symbols="")
