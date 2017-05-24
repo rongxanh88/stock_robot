e8e0bec9a960e930d4ebb291b16185fe8203214a
rongxanh88 <rongxanh88@gmail.com>
2017-05-24 08:48:17 -0600

    add security spec tests

--- spec/models/security_spec.rb
+++ spec/models/security_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Security, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "valid attributes" do
+    it { should validate_presence_of(:security_type) }
+    it { should validate_uniqueness_of(:security_type) }
+  end
+
+  describe "relationships" do
+    it "has many tickers" do
+      security = create(:security)
+      expect(security).to respond_to(:tickers)
+    end
+  end
 end
